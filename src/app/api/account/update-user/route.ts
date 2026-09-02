da5437761add595baff0bca37dbe9528b6ca8c73
rhinc <rhinchoe0106@gmail.com>
2026-09-03 03:10:22 +0800

    Delete org

--- src/app/api/account/update-user/route.ts
+++ src/app/api/account/update-user/route.ts
@@ -469,7 +469,7 @@ export async function POST(req: Request) {
           });
         }
       }
-    });
+    }, { maxWait: 10000, timeout: 30000 });
 
     const adminDemoted = editingSelf && wasAdmin && !isAdminAfterSave;
 
